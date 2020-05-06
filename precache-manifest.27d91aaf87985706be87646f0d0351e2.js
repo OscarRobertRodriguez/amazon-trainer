9753d47263c5a053c8fef0e61d33f60acaa2a71b
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2020-05-05 19:25:39 -0500

    Updates

---
+++ precache-manifest.27d91aaf87985706be87646f0d0351e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017dff00545467ca08cf7c8a106bc3a2",
+    "url": "/oscarrobertrodriguez.github.io/amazon-trainer/index.html"
+  },
+  {
+    "revision": "9c6208fe5ad10413a123",
+    "url": "/oscarrobertrodriguez.github.io/amazon-trainer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d8df2ea222bb1fc7e0c7",
+    "url": "/oscarrobertrodriguez.github.io/amazon-trainer/static/js/2.4b58da36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oscarrobertrodriguez.github.io/amazon-trainer/static/js/2.4b58da36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6208fe5ad10413a123",
+    "url": "/oscarrobertrodriguez.github.io/amazon-trainer/static/js/main.1f487538.chunk.js"
+  },
+  {
+    "revision": "6a7c87886ea839a148a6",
+    "url": "/oscarrobertrodriguez.github.io/amazon-trainer/static/js/runtime-main.059b73c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oscarrobertrodriguez.github.io/amazon-trainer/static/media/logo.5d5d9eef.svg"
+  }
+]);
