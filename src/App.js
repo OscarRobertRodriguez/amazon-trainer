8b8bd0f1384ec60e5f8a5508c21b970c3cc2dd1a
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2020-05-05 23:37:23 -0500

    setup initial styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../node_modules/normalize.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   
+       
     </div>
   );
 }
